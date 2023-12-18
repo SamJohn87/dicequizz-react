1eb857e3f14575f31c1a917485655961ececb9ad
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2023-12-17 22:09:20 -0600

    gameObjective page done

--- src/pages/GameObjective.js
+++ src/pages/GameObjective.js
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { useSpring, animated } from "@react-spring/web";
 import { Container, Col, Row, Card, CardBody, CardImg } from "reactstrap";
-import quizIcon from '../assets/images/shutterstock_2350330561.jpg';
+import diceIcon from '../assets/images/shutterstock-739407751-657fc278005ea.jpg';
+import playButton from '../assets/images/play.png';
 
 const GameObjective = () => {
     const card1Animation = useSpring({
@@ -22,7 +23,7 @@ const GameObjective = () => {
         delay: 1500
     });
 
-    const logoAnimation = useSpring({
+    const playAnimation = useSpring({
         from: { opacity: 0, },
         to: { opacity: 1 },
         delay: 700,
@@ -33,53 +34,63 @@ const GameObjective = () => {
     return (
         <Container fluid>
             <Row>
-                <Col className='theme rounded-4 text-center text-white fs-1 m-4 p-3 fw-bold shadow-lg'>Welcome to DiceQuizz Trivia</Col>
+                <Col className='theme rounded-4 text-center text-white fs-1 m-3 p-3 fw-bold shadow-lg'>Welcome to DiceQuizz Trivia</Col>
             </Row>
-            <Row fluid className='bg-white rounded d-flex align-items-center p-5 m-3'>
-                {/* <Col className='theme rounded-4 text-center text-white fs-3 p-5 m-4'>
-                    <p>The objective of the game is to answer trivia questions correctly and earn points.</p>
-                    <p>The sum of the two dice rolled not only determines the points but also the time available to answer each question.</p>
-                    <p>The player with the most points at the end wins.</p>
-                    <Link to={'gameselection'}>
-                        <button className='btn-custom fs-5 text-white fw-bold shadow-lg'>Let's Go!</button>
-                    </Link>
-                </Col> */}
+            <Row fluid className='bg-white rounded d-flex align-items-center p-5 m-2'>
                 <Col>
-                    <animated.div style={logoAnimation}>
-                        <Card className='border-0'>
-                            <CardImg
-                                src={quizIcon}
-                                alt='quiz icon'
-                            />
-                        </Card>
-                    </animated.div>
-                </Col>
-                <Col className='m-2'>
-                    <animated.div style={card1Animation}>
-                        <Card className='border border-3 shadow-lg h-50' >
-                            <CardBody className='p-5 fw-bold text-center fs-5 bg-light'>
-                                Roll the dice, answer the question correctly, and earn points.
-                            </CardBody>
-                        </Card>
-                    </animated.div>
-                </Col>
-                <Col className='m-2'>
-                    <animated.div style={card2Animation}>
-                        <Card className='border border-3 shadow-lg h-50'>
-                            <CardBody className='p-5 fw-bold text-center fs-5 bg-light' >
-                                The sum of the two dice rolled not only determines the points but also the time available to answer each question.
-                            </CardBody>
-                        </Card>
-                    </animated.div>
-                </Col>
-                <Col className='m-2'>
-                    <animated.div style={card3Animation}>
-                        <Card className='border border-3 shadow-lg h-50'>
-                            <CardBody className='p-5 fw-bold text-center fs-5 bg-light'>
-                                The player with the most points at the end wins.
-                            </CardBody>
-                        </Card>
-                    </animated.div>
+                    <Row className="d-flex align-items-center">
+                        <Col>
+                            <Card className='border-0'>
+                                <CardImg
+                                    src={diceIcon}
+                                    alt='quiz icon'
+                                />
+                            </Card>
+                        </Col>
+                        <Col className='m-2'>
+                            <animated.div style={card1Animation}>
+                                <Card className='border border-3 shadow-lg h-50' >
+                                    <CardBody className='p-5 fw-bold text-center fs-5 bg-light'>
+                                        Roll the dice, answer the question correctly, and earn points.
+                                    </CardBody>
+                                </Card>
+                            </animated.div>
+                        </Col>
+                        <Col className='m-2'>
+                            <animated.div style={card2Animation}>
+                                <Card className='border border-3 shadow-lg h-50'>
+                                    <CardBody className='p-5 fw-bold text-center fs-5 bg-light' >
+                                        The sum of the two dice rolled not only determines the points but also the time available to answer each question.
+                                    </CardBody>
+                                </Card>
+                            </animated.div>
+                        </Col>
+                        <Col className='m-2'>
+                            <animated.div style={card3Animation}>
+                                <Card className='border border-3 shadow-lg h-50'>
+                                    <CardBody className='p-5 fw-bold text-center fs-5 bg-light'>
+                                        The player with the most points at the end wins.
+                                    </CardBody>
+                                </Card>
+                            </animated.div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className='d-flex flex-row-reverse'>
+                            <Link to={'gameselection'}>
+                                <animated.div style={playAnimation}>
+                                    <Card className='border-0'>
+                                        <CardImg
+                                            src={playButton}
+                                            alt='play button'
+                                            width='50px'
+                                            height='50px'
+                                        />
+                                    </Card>
+                                </animated.div>
+                            </Link>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
