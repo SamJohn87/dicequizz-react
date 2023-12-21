dcfeb4b7b886f60ed7e39bce43301e6d5609f7b6
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2023-12-21 00:36:02 -0600

    fixed image view responsiveness page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,17 +3,16 @@ import Pagenotfound from '../assets/images/kostiantyn-li-Fi_nhg5itCw-unsplash.jp
 
 const PageNotFound = () => {
     return (
-        <Container fluid className='bg-white'>
+        <Container className='vh-100'>
             <Row>
-                <Col>
+                <Col className='bg-white'>
                     <img
                         src={Pagenotfound}
                         alt='Page Not Found'
+                        className='img-fluid'
                     />
+                    Image by <a href='https://www.linkedin.com/in/leedsn/' target='_blank' rel='noreferrer'>Kostiantyn Li</a>.
                 </Col>
-            </Row>
-            <Row>
-                <Col className='small'>Image by <a href='https://www.linkedin.com/in/leedsn/' target='_blank' rel='noreferrer'>Kostiantyn Li</a>.</Col>
             </Row>
         </Container>
     )
